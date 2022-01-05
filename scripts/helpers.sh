0e9d6d07d9fe25d076b49946e439e8a43729afba
jaclu <jacob.lundqvist@gmail.com>
2022-01-05 23:54:15 +0100

    Revert "quoted params"
    This reverts commit 33c69dac6e39f297eed54c950a827c7225678c2f.

--- scripts/helpers.sh
+++ scripts/helpers.sh
@@ -4,8 +4,8 @@ max_cache_time=5
 
 
 get_tmux_option() {
-    local option="$1"
-    local default_value="$2"
+    local option=$1
+    local default_value=$2
     local option_value
 
     option_value="$(tmux show-option -gqv "$option")"

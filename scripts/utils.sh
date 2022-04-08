bc09cb7c3374d806b6d5a9b63f4650c713a3d30a
jaclu <jacob.lundqvist@gmail.com>
2022-04-08 20:21:25 +0200

    renamed common stuff

--- scripts/utils.sh
+++ scripts/utils.sh
@@ -18,14 +18,6 @@ get_tmux_option() {
 }
 
 
-set_tmux_option() {
-    local option=$1
-    local value=$2
-
-    tmux set-option -gq "$option" "$value"
-}
-
-
 #
 #  Caching of status for max_cache_time seconds
 #
@@ -105,51 +97,6 @@ trim() {
 }
 
 
-color_statement() {
-    local fg
-    local bg
-
-    fg="$(trim "$1")"
-    bg="$(trim "$2")"
-
-    if [ -z "$fg" ] && [ -z "$bg" ]; then
-        return
-    elif [ -n "$fg" ] && [ -n "$bg" ]; then
-        echo "#[fg=$fg,bg=$bg]"
-    elif [ -n "$fg" ] && [ -z "$bg" ]; then
-        echo "#[fg=$fg]"
-    elif [ -z "$fg" ] && [ -n "$bg" ]; then
-        echo "#[bg=$bg]"
-    fi
-}
-
-
-mullvad_status_colors() {
-    status="$(mullvad_status| grep status | awk '{print $3}')"
-
-    #
-    #  To reduce overhead, only those colors actually needed are read from tmux
-    #
-    if [[ $status == "Disconnected" ]]; then
-        disconnected_fg_color=$(get_tmux_option "@mullvad_disconnected_fg_color")
-        disconnected_bg_color=$(get_tmux_option "@mullvad_disconnected_bg_color" "red")
-        color_statement "$disconnected_fg_color" "$disconnected_bg_color"
-    elif [[ $status == "Connecting" ]]; then
-        connecting_fg_color=$(get_tmux_option "@mullvad_connecting_fg_color")
-        connecting_bg_color=$(get_tmux_option "@mullvad_connecting_bg_color" "yellow")
-        color_statement "$connecting_fg_color" "$connecting_bg_color"
-    elif [[ $status == "Connected" ]]; then
-        connected_fg_color=$(get_tmux_option "@mullvad_connected_fg_color")
-        connected_bg_color=$(get_tmux_option "@mullvad_connected_bg_color" "green")
-        color_statement "$connected_fg_color" "$connected_bg_color"
-    else #  Default to Blocked color, to show something is off
-        blocked_fg_color=$(get_tmux_option "@mullvad_blocked_fg_color")
-        blocked_bg_color=$(get_tmux_option "@mullvad_blocked_bg_color" "purple")
-        color_statement "$blocked_fg_color" "$blocked_bg_color"
-    fi
-}
-
-
 #
 #  If color is defined for current status, wrap text in a color statement
 #
@@ -196,30 +143,3 @@ is_excluded_country() {
 }
 
 
-is_excluded_city() {
-    local excluded_city
-
-    excluded_city=$(get_tmux_option "@mullvad_excluded_city")
-
-    # not local, can be used by caller
-    city="$(mullvad_status | grep Location | cut -d' ' -f2- | cut -d',' -f1)"
-
-    case "$city" in
-
-        *"navailable"*)
-            #
-            # Fake excluded, to avoid the Location unavailable Prompt that
-            # sometimes come up during connection.
-            #
-            return 0
-            ;;
-
-        "$excluded_city")
-            return 0
-            ;;
-
-        *)
-            return 1
-            ;;
-    esac
-}

b094b20c6781c36ae10cc43737dc352f8c90c264
jaclu <jacob.lundqvist@gmail.com>
2022-04-14 12:11:29 +0200

    made it more informative

--- shellchecker.sh
+++ shellchecker.sh
@@ -1,20 +1,34 @@
-#!/bin/sh
+#!/usr/bin/env bash
 #
-#   Copyright (c) 2022: Jacob.Lundqvist@gmail.com
-#   License: MIT
+#  Copyright (c) 2022: Jacob.Lundqvist@gmail.com
+#  License: MIT
 #
 #  Part of https://github.com/jaclu/tmux-mullvad
 #
-#   Version: 1.0.0 2022-04-14
+#  Version: 1.0.1 2022-04-14
 #
-#   Does shellcheck on all relevant scripts in this project
+#  Does shellcheck on all relevant scripts in this project
 #
 
-# shellcheck disable=SC1007
+#  shellcheck disable=SC1007
 CURRENT_DIR=$(CDPATH= cd -- "$(dirname -- "$0")" && pwd)
 
-#  Obviousl self exam should be done :)
-shellcheck "$CURRENT_DIR"/shellchecker.sh
+cd "$CURRENT_DIR" || return
 
-shellcheck "$CURRENT_DIR"/mullvad.tmux
-shellcheck "$CURRENT_DIR"/scripts/*.sh
+
+checkables=(
+
+    #  Obviousl self exam should be done :)
+    shellchecker.sh
+
+    mullvad.tmux
+
+    scripts/*.sh
+)
+
+for script in "${checkables[@]}"; do
+    # abort as soon as one gives warnings
+    echo "Checking: $script"
+    shellcheck -x "$script" || exit 1
+
+done
